04d241c8c3e88e69cca1e7e8106f565d9ef0ccf1
Caleb Copeland <calebcopeland1@gmail.com>
2021-04-07 14:42:03 -0400

    Initial sharing of project

---
+++ ShapeMaker.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class ShapeMaker here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ShapeMaker
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class ShapeMaker
+     */
+    public ShapeMaker()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
