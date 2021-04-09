124384d44a03be70021a3fd82bf1b6da57cc57a7
c <c@>
2021-04-09 14:06:01 -0400

    level

---
+++ Level.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Level here.
+ *
+ * @author Caleb Copeland
+ * @version 4/9/21
+ * @since 4/9/21
+ */
+public class Level
+{
+    // instance variables - replace the example below with your own
+    public AlgoShape[] shapes;
+    public GameObject[] entities;
+    
+    public Level 
+}
